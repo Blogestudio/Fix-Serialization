41025ad0864828553dcca99422ff0cec22e1e115
Pau Iglesias <pauiglesias@gmail.com>
2011-12-15 01:38:30 +0100

    Second version 1.2

--- fix-serialization.php
+++ fix-serialization.php
@@ -2,11 +2,11 @@
 <?php
 
 /*
-* Blogestudio Fix Serialization	1.1
+* Blogestudio Fix Serialization	1.2
 * http://blogestudio.com
 * By Pau Iglesias
 *
-* Fixer script of length attributes for serialized strings (e.g. WP databases)
+* Fixer script of length attributes for serialized strings (e.g. Wordpress databases)
 *
 * Usage:
 *
@@ -14,18 +14,27 @@
 *
 * Inspiration and regular expression code base from David Coveney: 
 * http://davidcoveney.com/575/php-serialization-fix-for-wordpress-migrations/
-* 
+*
 * Versions:
 * 
 * 	1.0 2011-08-03 Initial release
 * 	1.1 2011-08-18 Support for backslashed quotes, added some code warnings
-*
+* 	1.2 2011-09-29 Support for null or zero length strings after preg_replace is called, and explain how to handle these errors
+* 
 * Knowed errors:
 *
 * - Memory size exhausted
 * Allowed memory size of 67108864 bytes exhausted (tried to allocate 35266489 bytes)
-* How to fix: update php.ini memory_limit to 512M or more, and restart cgi service or web server.
+* How to fix: update php.ini memory_limit to 512M or more, and restart cgi service or web server
 *
+* - Function preg_replace returns null or 0 length string
+* If preg_last_error = PREG_BACKTRACK_LIMIT_ERROR (value 2), increase pcre.backtrack_limit in php.ini (by default 100k, change to 2M by example)
+* Same way for others preg_last_error codes: http://www.php.net/manual/en/function.preg-last-error.php
+* 
+* TODO next versions
+* 
+* - Check if needed UTF-8 support detecting and adding u PCRE modifier
+* 
 */
 
 
@@ -42,7 +51,7 @@ function unescape_mysql($value) {
 // Fix strange behaviour if you have escaped quotes in your replacement
 function unescape_quotes($value) {
 	return str_replace('\"', '"', $value);
-}
+}	
 
 
 
@@ -75,17 +84,30 @@ if (!(isset($argv) && isset($argv[1]))) {
 		
 		// File opened for read
 		} else {
+			
+			// Initializations
+			$do_preg_replace = false;
 		
 			// Copy data
 			if (!($data = fread($fp, filesize($path)))) {
 
 				// Error
 				echo 'Error: can`t read entire data from input file'."\n";
-				echo $path."\n\n";				
+				echo $path."\n\n";
+			
+			// Check data
+			} elseif (!(isset($data) && strlen($data) > 0)) {
+
+				// Warning
+				echo "Warning: the file is empty or can't read contents\n";
+				echo $path."\n\n";
 			
 			// Data ok
 			} else {
-			
+
+				// Tag context
+				$do_preg_replace = true;
+
 				// Replace serialized string values
 				$data = preg_replace('!s:(\d+):([\\\\]?"[\\\\]?"|[\\\\]?"((.*?)[^\\\\])[\\\\]?");!e', "'s:'.strlen(unescape_mysql('$3')).':\"'.unescape_quotes('$3').'\";'", $data);
 			}
@@ -93,26 +115,42 @@ if (!(isset($argv) && isset($argv[1]))) {
 			// Close file
 			fclose($fp);
 			
-			// And finally write data
-			if (!($fp = fopen($path, 'w'))) {
+			// Check data
+			if (!(isset($data) && strlen($data) > 0)) {
+				
+				// Check origin
+				if ($do_preg_replace) {
 
-				// Error
-				echo "Error: can't open input file for writing\n";
-				echo $path."\n\n";
-				
-			// Open for write
-			} else {
-				
-				// Write file data
-				if (!fwrite($fp, $data)) {
-					
 					// Error
-					echo "Error: can't write input file\n";
+					echo "Error: preg_replace returns nothing\n";
+					if (function_exists('preg_last_error')) echo "preg_last_error() = ".preg_last_error()."\n";
 					echo $path."\n\n";
 				}
-				
-				// Close file
-				fclose($fp);
+			
+			// Data Ok
+			} else {
+
+				// And finally write data
+				if (!($fp = fopen($path, 'w'))) {
+
+					// Error
+					echo "Error: can't open input file for writing\n";
+					echo $path."\n\n";
+					
+				// Open for write
+				} else {
+					
+					// Write file data
+					if (!fwrite($fp, $data)) {
+						
+						// Error
+						echo "Error: can't write input file\n";
+						echo $path."\n\n";
+					}
+					
+					// Close file
+					fclose($fp);
+				}
 			}
 		}
 	}
